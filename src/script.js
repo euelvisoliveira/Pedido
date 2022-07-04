2968f03d329b90ced149d1de912930c355fd0ee2
Elvis Ribeiro <elvis.ribeiro.oliveira@hotmail.com>
2022-07-04 14:41:22 -0300

    aprendo-jquery

--- src/script.js
+++ src/script.js
@@ -1,17 +1,59 @@
-const handleClick = () => {
-  const body = document.querySelector('body')
-  body.innerHTML = `
-  <h1>Hello World</h1>
-  `
+//-------- IMPLEMENTACAO EM JAVASCRIPT NORMAL------------//
 
-  body.style.backgroundColor = '#000'
-  body.style.color = '#FFF'
+// const handleClick = () => {
+//   const body = document.querySelector('body')
+//   body.innerHTML = `
+//     <h1>Hello World</h1>
+//     `
+
+//   body.style.backgroundColor = '#000'
+//   body.style.color = '#FFF'
+// }
+// document.addEventListener('DOMContentLoaded', () => {
+//   const el = document.getElementById('btn-start')
+//   el.addEventListener('mouseover', () => {
+//     el.style.position = 'absolute'
+//     el.style.top = `${Math.floor(Math.random() * (window.innerHeight - 18))}px`
+//     el.style.left = `${Math.floor(Math.random() * (window.innerWidth - 36))}px`
+//   })
+//   el.addEventListener('click', () => handleClick())
+// })
+
+//-------- IMPLEMENTACAO EM JQUERY------------//
+
+const handleClick = () => {
+  const body = $('body')
+  body.html(`<h1>Hello World</h1>`)
+
+  body.css('backgroundColor', '#000')
+  body.css('color', '#FFF')
 }
 
-const el = document.getElementById('btn-start')
-el.addEventListener('mouseover', () => {
-  el.style.position = 'absolute'
-  el.style.top = `${Math.floor(Math.random() * (window.innerHeight - 18))}px`
-  el.style.left = `${Math.floor(Math.random() * (window.innerWidth - 36))}px`
+const startButtonInit = () => {
+  const el = $('#btn-start')
+  el.on('mouseover', () => {
+    el.css('position', 'absolute')
+    el.css('top', `${Math.floor(Math.random() * (window.innerHeight - 18))}px`)
+    el.css('left', `${Math.floor(Math.random() * (window.innerWidth - 36))}px`)
+  })
+  el.on('click', () => handleClick())
+}
+
+const infoButtonInit = () => {
+  const el = $('#btn-info')
+  const elIntiallText = el.html()
+  el.on('click', () => {
+    if (el.html() == elIntiallText) {
+      el.html('OCULTAR INFORMAÇÕES')
+    } else {
+      el.html(elIntiallText)
+    }
+
+    $('.info').slideToggle()
+  })
+}
+
+$(document).ready(() => {
+  startButtonInit()
+  infoButtonInit()
 })
-el.addEventListener('click', () => handleClick())
